c74e214fa7dd91529c8456f54cd94b9bfd14ebba
Koshelev <koschelev00@mail.ru>
2025-08-05 13:12:17 +0300

    Пределал hw-2

--- src/app.js
+++ src/app.js
@@ -1,65 +1,52 @@
-const http = require('http');
-const url= require('url');
-const getUsers = require('./modules/users');
+const http = require("http");
+const url = require("url");
+const getUsers = require("./modules/users");
 
-const server = http.createServer((request, response ) => {
-    const parsedUrl = url.parse(request.url, true);
-    const pathName = parsedUrl.pathname;
-    
-    if (pathName === '/users') {
-    response.status =200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type : application/json"
+const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+  const pathName = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  if (pathName === "/users") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
     response.write(getUsers());
     response.end();
     return;
+  }
+  if ("hello" in query) {
+    const nameValue = query.hello;
+
+    if (nameValue && nameValue.trim() !== "") {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.write(`Hello, ${nameValue}!`);
+      response.end();
+    } else {
+      response.statusCode = 400;
+      response.setHeader("Content-Type", "text/plain");
+      response.write("Enter a name");
+      response.end();
     }
-
-   
-
-if (pathName === '/hello') {
- const name = parsedUrl.query.name;
-   if(name){
-      response.status =200;
-      response.statusMessage = "OK";
-      response.header = "Content-Type : text/plain"
-      response.write(`Hello, ${name}!`);
-      response.end();
-   }else {
-      response.status =400;
-      response.statusMessage = "Bad Request";
-      response.header = "Content-Type : text/plain"
-      response.write(`Enter a name `);
-      response.end();
-      
-   }
-   return;
-}
-
-  if (pathName === '/') {
-    response.status =200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type : text/plain"
-    response.write("Hello, world!");
+    return;
+  }
+  if (pathName === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.write("Hello, World!");
     response.end();
- }else{
-
-   response.statusCode = 500;
-   response.statusMessage = "OK";
-   response.header = "Content-Type : text/plain"
-   response.write(" ");
-   response.end();
- }
-  
+  } else {
+    response.statusCode = 500;
+    response.end();
+  }
 });
 
-server.listen(3003, () =>{
-    console.log("Сервер запущен по адрессу http://127.0.0.1:3003")
-})
+const port = process.env.PORT || 3003;
+server.listen(port, () => {
+  console.log(`Сервер запущен по адресу http://127.0.0.1:${port}`);
+});
 
-
-
-// // http://127.0.0.1:3003/  response:'Hello world'
+// http://127.0.0.1:3003/  response:'Hello world'
 // http://127.0.0.1:3003/users  response:users.json
-// http://127.0.0.1:3003/hello   response: 'Enter a name' 
-// http://127.0.0.1:3003/hello?mame="Name"  response: 'Hello, 'Name'!'
+// http://127.0.0.1:3003/?hello   response: 'Enter a name'
+// http://127.0.0.1:3003/?hello="Name"  response: 'Hello, 'Name'!'
